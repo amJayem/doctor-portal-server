e6e15c94066dfe40ea9fb2ad82836861e86687a5
amJayem <csejayem19@gmail.com>
2022-11-16 17:06:22 +0600

    booking saved

--- index.js
+++ index.js
@@ -12,16 +12,39 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
         const appointmentOptionCollection = client.db('doctor-portal').collection('appointment-options');
-        
+        const bookingCollection = client.db('doctor-portal').collection('bookings');
+
+        app.get('/appointmentOptions', async(req,res)=>{
+            const query = {}
+            const result = await appointmentOptionCollection.find(query).toArray();
+
+            res.send(result);
+        });
+
+        app.post('/bookings', async(req, res)=>{
+            const booking = req.body;
+            console.log(booking);
+            // const query = {
+            //     appointmentDate: booking.appointmentDate,
+            //     email: booking.email,
+            //     treatment: booking.treatment
+            // }
+
+            const result = await bookingCollection.insertOne(booking);
+
+            res.send(result);
+        })
+
     }
     finally {}
 }
-
+run().catch(e=>console.error('run error => ', e))
 
 app.get('/',async (req,res)=>{
     res.send('doctor portal is running...');
