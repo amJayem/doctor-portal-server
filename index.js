899d461d36a3ffea9af04af62e84a0cd81b81a76
amJayem <csejayem19@gmail.com>
2022-11-17 21:33:19 +0600

    jwt verified success

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req,res,next){
+    // console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send(`Unauthorized access`);
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token,process.env.TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run(){
     try{
         const appointmentOptionCollection = client.db('doctor-portal').collection('appointment-options');
@@ -44,9 +63,14 @@ async function run(){
             res.send(options);
         });
 
-        app.get('/bookings', async(req,res)=>{
+        app.get('/bookings',verifyJWT, async(req,res)=>{
             const email = req.query.email;
             const query = {email: email};
+            const decodedEmail = req.decoded.email;
+
+            if(email !== decodedEmail){
+                return res.status(403).send({message: 'forbidden access'});
+            }
             const bookings = await bookingsCollection.find(query).toArray();
             
             res.send(bookings);
